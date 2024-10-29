fd5dfe484e48cadee4fc8d5f812fecaf240c1df0
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-10-29 15:57:24 +0100

    Final paso 4

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -12,7 +13,7 @@ class Product extends Model
 
     protected $fillable = ['name', 'description', 'price'];
 
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
