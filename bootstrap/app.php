d202f69ab73fbe809c5e80b008e7cb70b710d405
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-11-20 16:34:59 +0100

    final paso 20

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
